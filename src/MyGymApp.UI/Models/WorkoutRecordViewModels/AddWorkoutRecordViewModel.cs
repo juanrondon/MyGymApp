c53c283f6e2dbbecc57987b6de2b1c0bccbe783d
Juan Rondon <juanrondonc@outlook.com>
2017-02-24 09:03:34 +1100

    Added view for create workout-record

--- src/MyGymApp.UI/Models/WorkoutRecordViewModels/AddWorkoutRecordViewModel.cs
+++ src/MyGymApp.UI/Models/WorkoutRecordViewModels/AddWorkoutRecordViewModel.cs
@@ -5,11 +5,13 @@ namespace MyGymApp.UI.Models.WorkoutRecordViewModels
 {
     public class AddWorkoutRecordViewModel
     {
+        [Required]
         [Display(Name = "Activity Name")]
         public string ActivityName { get; set; }
         public int Duration { get; set; }
         public int Sets { get; set; }
         public int Reps { get; set; }
+        [Required]
         public int[] MusclesListIds { get; set; }
         public MultiSelectList MusclesList { get; set; }
     }
